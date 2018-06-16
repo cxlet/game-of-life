fff1809724c932c9a15243a06d2f548b08b80858
cxlet <cxlet@outlook.com>
2018-06-16 23:25:14 +0800

    Modify all project names to game-of-life.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -6,7 +6,7 @@
 
 <script>
   export default {
-    name: 'life-game'
+    name: 'game-of-life'
   }
 </script>
 
--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -7,7 +7,7 @@ describe('Launch', function () {
   it('shows the proper application title', function () {
     return this.app.client.getTitle()
       .then(title => {
-        expect(title).to.equal('life-game')
+        expect(title).to.equal('game-of-life')
       })
   })
 })
